fae096e2ce9fcd9e5938d63707badb3499bb4f56
Pedro <pedro_mello@icloud.com>
2024-11-04 17:04:28 -0300

    eslint error

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // ... other config
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'warn', // changes error to warning
-    // or
-    '@typescript-eslint/no-unused-vars': 'off' // disables the rule completely
-  }
-} 
